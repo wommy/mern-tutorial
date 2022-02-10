b663c47c508e922668f851f51f781d4b31b60553
wommy <tom@technomad.media>
2022-02-10 15:20:21 -0500

    init: errorHandler

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 express()
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
   .use('/api/goals/', require('./routes/goalRoutes'))
+  .use(errorHandler)
   .listen(port, () => console.log(`server started on port ${port}`))
