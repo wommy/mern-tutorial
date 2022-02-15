f0dbf0bb1302c4c32648322976966bb381317b8b
wommy <tom@technomad.media>
2022-02-15 10:22:35 -0500

    Login form

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -10,6 +10,14 @@ const register = async userData => {
   return response.data
 }
 
+const login = async userData => {
+  const response = await axios.post(API_URL + 'login', userData)
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+  return response.data
+}
+
 const logout = () => {
   localStorage.removeItem('user')
 }
@@ -17,6 +25,7 @@ const logout = () => {
 const authService = {
   register,
   logout,
+  login,
 }
 
 export default authService
